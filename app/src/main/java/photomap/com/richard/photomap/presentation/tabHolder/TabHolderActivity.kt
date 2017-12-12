c2c7c8e627fee47a0144d0622ce157f1fc238e40
Ричард Мацкевич <mazkewich777@gmail.com>
2017-12-12 18:13:54 +0300

    Configure bottom navigation bar

--- app/src/main/java/photomap/com/richard/photomap/presentation/tabHolder/TabHolderActivity.kt
+++ app/src/main/java/photomap/com/richard/photomap/presentation/tabHolder/TabHolderActivity.kt
@@ -3,6 +3,7 @@ package photomap.com.richard.photomap.presentation.tabHolder
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_tab_holder.*
 import photomap.com.richard.photomap.R
 
@@ -10,16 +11,14 @@ class TabHolderActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_map -> {
+                message.setText(R.string.title_map)
+                Log.d("Photo Map", "Map tab")
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_photo_list -> {
+                message.setText(R.string.title_photo_list)
+                Log.d("Photo Map", "Photo list tab")
                 return@OnNavigationItemSelectedListener true
             }
         }
