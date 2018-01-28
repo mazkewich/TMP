3cbab32ad787a54ab0270cfe5ab2d50583f22e3f
Ричард Мацкевич <mazkewich777@gmail.com>
2018-01-28 20:34:55 +0300

    Add button to the map

--- app/src/main/java/photomap/com/richard/photomap/presentation/map/MapFragment.kt
+++ app/src/main/java/photomap/com/richard/photomap/presentation/map/MapFragment.kt
@@ -5,14 +5,16 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.maps.*
+import kotlinx.android.synthetic.main.fragment_map.*
 
 import photomap.com.richard.photomap.R
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mapView: MapView
@@ -75,6 +77,42 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        categoryButton.setOnClickListener(this)
+        locationButton.setOnClickListener(this)
+        cameraButton.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == null) {
+            return
+        }
+
+        when (p0.id) {
+            locationButton.id -> locationAction()
+            cameraButton.id -> cameraAction()
+            categoryButton.id -> categoryAction()
+            else -> {
+                Log.w("Photo Map", "Other button was clicked")
+            }
+        }
+    }
+
+    private fun locationAction() {
+        locationButton.isSelected = !locationButton.isSelected
+        Log.w("Photo Map", "locationAction")
+    }
+
+    private fun cameraAction() {
+        Log.w("Photo Map", "cameraAction")
+    }
+
+    private fun categoryAction() {
+        Log.w("Photo Map", "categoryAction")
     }
 
     override fun onDetach() {
